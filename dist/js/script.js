fe621d4f9183d41e516df39fd1d81b183575f566
Salmon-Andre19 <salmon.andri19@gmail.com>
2025-02-27 14:04:36 +0700

    menambahkan mode dark mode dan menambahkan konfigurasi pada input.css

--- dist/js/script.js
+++ dist/js/script.js
@@ -31,14 +31,24 @@ window.addEventListener("click", function (e) {
   }
 });
 
-// // Dark mode toggle
-// const darkToggle = document.querySelector("#dark-toggle");
-// const html = document.querySelector("html");
+// Dark mode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
 
-// darkToggle.addEventListener("click", function () {
-//   if (darkToggle.checked) {
-//     html.classList.add("dark");
-//   } else {
-//     html.classList.remove("dark");
-//   }
-// });
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = 'dark';
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = 'light';
+  }
+});
+
+
+// pindahkan posisi toggle sesuai mode
+if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.cheked = false;
+};
